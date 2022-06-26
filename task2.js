76f816c670e8c35992ee59e964579bdd1f453853
MURUGAN SEKAR <murugansekar2608@gmail.com>
2022-06-26 16:11:31 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,7 @@
+const product = (x,y) => x*y
+console.log(product(2,3))
+
+const student = {id:7,name:'Murugan',
+display(){console.log("Id is "+this.id+" and Name is "+this.name)}
+}
+student.display()
